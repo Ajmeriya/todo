ccfee48f814803325eca373d2fa05cf0fbc81a66
Harsh Ajmeriya <ajmeriyaharsh932@gmail.com>
2026-07-17 20:40:28 +0530

    hello

--- backend/src/main/java/com/example/todo/TodoApplication.java
+++ backend/src/main/java/com/example/todo/TodoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TodoApplication {
 
-    public static void main() {
-        SpringApplication.run(TodoApplication.class);
-        
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplication.class, args);
     }
+    // hello
 }
